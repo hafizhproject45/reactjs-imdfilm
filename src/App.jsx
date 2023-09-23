ada873f48e62cd44a84725db917ac16a98376833
hafizh.project45 <hafizh.project45@gmail.com>
2023-09-23 21:16:00 +0700

    Fixing some bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import FooterComponent from "./components/FooterComponent";
 import Homepage from "./pages/homepage";
 const App = () => {
   return (
-    <>
+    <div>
       <Homepage />
       <FooterComponent />
-    </>
+    </div>
   );
 };
 
